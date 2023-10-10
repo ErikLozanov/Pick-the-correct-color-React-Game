cb8bcfdfc8a53bfa3bb3b395289e61fd4d781777
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-10 23:11:13 +0300

    changed dependency array and added func for random numbers

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,26 +1,29 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+function generateRandomColor () {
+  return Math.floor(Math.random()*16777215).toString(16).toUpperCase();
+}
+
 function App() {
   const [colors, setColors] = useState([]);
-  // const [wrongColor1, setWrongColor1] = useState('');
-  // const [wrongColor2, setWrongColor2] = useState('');
   const [randomNum,setRandomNum] = useState(Math.floor(Math.random() * 3));
-  const [nextColor, setNextColor] = useState(false);
+  const [nextColor,setNextColor] = useState(false);
   const [checker,setChecker] = useState();
 
   useEffect(() => {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16).toUpperCase();
-    let randomColor1 = Math.floor(Math.random()*16777215).toString(16).toUpperCase();
-    let randomColor2 = Math.floor(Math.random()*16777215).toString(16).toUpperCase();
+    setNextColor(false);
+    let randomColor = generateRandomColor();
+    let randomColor1 = generateRandomColor();
+    let randomColor2 = generateRandomColor();
     setColors(["#" + randomColor, '#' + randomColor1, "#" + randomColor2]);
-  },[randomNum]);
+  },[nextColor]);
 
-  const checkHandler = (e) => {
-    let pickedColor = e.currentTarget.value
+  const checkHandler = (pickedColor) => {
     if(pickedColor === colors[randomNum]) {
       setRandomNum(Math.floor(Math.random() * 3));
       setChecker('Correct')
+      setNextColor(true);
     } else {
       setChecker('Wrong')
     }
@@ -28,7 +31,7 @@ function App() {
   return (
     <>
       <div style={{backgroundColor: colors[randomNum], padding: '80px', margin: '40px'}}></div>
-      {colors.map(colorId => <button key={colorId} onClick={checkHandler} value={colorId} className='btn'>{colorId}</button>)}
+      {colors.map(colorId => <button key={colorId} onClick={() => checkHandler(colorId)} className='btn'>{colorId}</button>)}
      {checker == 'Correct' && <h3>Correct!</h3>}
      {checker == 'Wrong' && <h3>Wrong!</h3>}
     </>
